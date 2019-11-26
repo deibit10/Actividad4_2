3c561a3183e675cd495e08ed67cf049c25540395
David Sánchez López <davidsanchez.alu@iespacomolla.es>
2019-11-26 17:00:50 +0100

    "Empezamos la actividad con la primera versión del diagrama y la creación del proyecto"

---
+++ src/u4actividad2dsl/U4Actividad2DSL.java
@@ -0,0 +1,16 @@
+package u4actividad2dsl;
+
+/**
+ *
+ * @author David Sánchez López
+ */
+public class U4Actividad2DSL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
